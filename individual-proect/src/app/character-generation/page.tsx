797b0a47b7c93f1b4e0d3a3570c85009ab33f9ed
Nathaniel Dunham <dun19007@byui.edu>
2024-02-03 16:14:01 -0700

    started working on the layout of the project.

---
+++ individual-proect/src/app/character-generation/page.tsx
@@ -0,0 +1,22 @@
+'use client';
+import Image from "next/image";
+import { Button } from "../ui/button";
+import { useState } from 'react';
+import { LeftPanelButton } from "../ui/left-panel-button";
+import { LeftPanel } from "../ui/left-panel";
+
+export default function Home() {
+  
+
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-0 m-0">
+      <div className="flex min-w-full min-h-20 bg-gray-400">
+
+      </div>
+      <div className="relative grid min-w-full min-h-screen top-0 flex-col items-center justify-between p-0 m-0 sm:static sm:mainPageLayout">
+        <LeftPanel />
+      </div>
+
+    </main>
+  );
+}
